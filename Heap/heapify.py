f8ee729b16f7ea219f2219d8d9531038c4313c93
JunYoung Choi <cjy13753@gmail.com>
2021-01-09 00:26:43 +0900

    Add new python scripts for a new data structure, heap

---
+++ Heap/heapify.py
@@ -0,0 +1,53 @@
+def swap(tree, index_1, index_2):
+    temp = tree[index_1]
+    tree[index_1] = tree[index_2]
+    tree[index_2] = temp
+
+def heapify(tree, index, tree_size):
+
+    left_child_index = 2 * index
+    right_child_index = 2 * index + 1
+
+    largest = index
+
+    if 0 < left_child_index < tree_size and tree[largest] < tree[left_child_index]:
+        largest = left_child_index
+
+    if 0 < right_child_index < tree_size and tree[largest] < tree[right_child_index]:
+        largest = right_child_index
+    
+    if largest != index:
+        swap(tree, index, largest)
+        heapify(tree, largest, tree_size)
+
+    # My answer
+    # left_child_index = 2 * index
+    # right_child_index = 2 * index + 1
+    #
+    # if tree_size < left_child_index:
+    #     pass
+    # elif tree_size < right_child_index:
+    #     if tree[left_child_index] > tree[index]:
+    #         swap(tree, left_child_index, index)
+    #         heapify(tree, left_child_index, tree_size)
+    # else:
+    #     max_index = int
+    #     if tree[left_child_index] > tree[index]:
+    #         max_index = left_child_index
+    #         if tree[right_child_index]> tree[left_child_index]:
+    #             max_index = right_child_index
+    #     else:
+    #         max_index = index
+    #         if tree[right_child_index] > tree[index]:
+    #             max_index = right_child_index
+
+    #     if max_index != index:
+    #         swap(tree, max_index, index)
+    #         heapify(tree, max_index, tree_size)
+
+
+
+# Test code
+tree = [None, 15, 5, 12, 14, 9, 10, 6, 2, 11, 1]
+heapify(tree, 2, len(tree))
+print(tree)
---
+++ Heap/heapsort.py
@@ -0,0 +1,51 @@
+def swap(tree, index_1, index_2):
+    temp = tree[index_1]
+    tree[index_1] = tree[index_2]
+    tree[index_2] = temp
+
+
+def heapify(tree, index, tree_size):
+
+    left_child_index = 2 * index
+    right_child_index = 2 * index + 1
+
+    largest = index
+
+    if 0 < left_child_index < tree_size and tree[largest] < tree[left_child_index]:
+        largest = left_child_index
+
+    if 0 < right_child_index < tree_size and tree[largest] < tree[right_child_index]:
+        largest = right_child_index
+    
+    if largest != index:
+        swap(tree, index, largest)
+        heapify(tree, largest, tree_size)
+
+def heapsort(tree):
+    tree_size = len(tree)
+
+    for index in range(tree_size-1, 0 , -1):
+        heapify(tree, index, tree_size)
+
+    for i in range(tree_size-1, 1, -1):
+        swap(tree, 1, i)
+        heapify(tree, 1, i)
+
+# My answer
+# def heapsort(tree):
+#     tree_size = len(tree)
+
+#     for i in range(1, tree_size):
+#         index = tree_size - i
+#         heapify(tree, index, tree_size)
+
+#     for i in range(1, tree_size-1):
+#         index = tree_size - i
+#         swap(tree, 1, index)
+#         heapify(tree, 1, len(tree)-i)
+
+# Test code
+data_to_sort = [None, 6, 1, 4, 7, 10, 3, 8, 5, 1, 5, 7, 4, 2, 1]
+
+heapsort(data_to_sort)
+print(data_to_sort)
